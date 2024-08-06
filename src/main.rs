4a357eb303f9ae79c5cc43f7d77462517b8fddfe
Wachamuli <josemrr27@gmail.com>
2024-08-06 16:02:22 -0400

    the battery is displayed properly in the panel

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 mod binding;
-mod panel;
 mod styling;
 mod widget;
 
 use iced::{
-    widget::{container, text},
-    Command, Element,
+    widget::{column, container, row, svg, text},
+    Alignment, Command, Element,
 };
-
 use iced_layershell::{reexport::Anchor, Application as _};
+use styling::font::SF_PRO_BOLD;
 
-struct Bar {
+struct ControlCenter {
+    is_charging: bool,
     percentage: f64,
+    time_to_empty: i64,
 }
 
 #[derive(Debug, Clone)]
@@ -19,14 +20,21 @@ enum Message {
     UPowerDevice(binding::upower::BatteryInfo),
 }
 
-impl iced_layershell::Application for Bar {
+impl iced_layershell::Application for ControlCenter {
     type Executor = iced::executor::Default;
     type Message = Message;
     type Theme = styling::theme::Theme;
     type Flags = ();
 
     fn new(_flags: ()) -> (Self, Command<Message>) {
-        (Self { percentage: 100.0 }, Command::none())
+        (
+            Self {
+                percentage: 100.0,
+                time_to_empty: 0,
+                is_charging: false,
+            },
+            Command::none(),
+        )
     }
 
     fn namespace(&self) -> String {
@@ -41,14 +49,18 @@ impl iced_layershell::Application for Bar {
         match message {
             Message::UPowerDevice(event) => {
                 match event {
+                    binding::upower::BatteryInfo::NotAvailable => {
+                        todo!("What to do when there's no battery in the system?")
+                    }
                     binding::upower::BatteryInfo::Available {
                         is_charging,
                         percent,
                         time_to_empty,
                     } => {
+                        self.is_charging = is_charging;
                         self.percentage = percent;
+                        self.time_to_empty = time_to_empty;
                     }
-                    binding::upower::BatteryInfo::NotAvailable => std::process::exit(0),
                 }
 
                 Command::none()
@@ -57,22 +69,40 @@ impl iced_layershell::Application for Bar {
     }
 
     fn view(&self) -> Element<Message, Self::Theme> {
-        container(text("Control Center"))
-            .width(iced::Length::Fill)
-            .height(iced::Length::Fill)
-            .style(styling::style::Container::HeavyRounded)
-            .into()
+        let battery_icon = format!(
+            "{}/assets/icons/battery-{}.svg",
+            env!("CARGO_MANIFEST_DIR"),
+            (self.percentage as i32 + 5) / 10 * 10
+        );
+
+        container(
+            row![
+                svg(svg::Handle::from_path(battery_icon))
+                    .width(25)
+                    .height(25),
+                column![
+                    text(format!("{}%", self.percentage)).font(SF_PRO_BOLD),
+                    text(format!("{} hours", self.time_to_empty / 3600))
+                ]
+            ]
+            .align_items(Alignment::Center),
+        )
+        .padding(32)
+        .width(iced::Length::Fill)
+        .height(iced::Length::Fill)
+        .style(styling::style::Container::HeavyRounded)
+        .into()
     }
 }
 
 fn main() -> Result<(), iced_layershell::Error> {
-    Bar::run(iced_layershell::settings::Settings {
+    ControlCenter::run(iced_layershell::settings::Settings {
         antialiasing: true,
         default_font: styling::font::SF_PRO,
         layer_settings: iced_layershell::settings::LayerShellSettings {
-            size: Some((0, 60)),
-            exclusize_zone: 40,
-            anchor: Anchor::Top | Anchor::Left | Anchor::Right,
+            size: Some((475, 375)),
+            anchor: Anchor::Right | Anchor::Top,
+            margins: (40 + 15, 10, 0, 0),
             ..Default::default()
         },
         ..Default::default()
--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -1,2 +1,1 @@
-pub mod battery;
 pub mod icon;
