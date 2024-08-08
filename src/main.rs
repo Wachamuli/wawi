9aae15278e937b3f5a2bb124acfaa59c362e24ac
Wachamuli <josemrr27@gmail.com>
2024-08-07 22:21:04 -0400

    use of closures to reuse buttons

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ mod styling;
 mod widget;
 
 use iced::{
-    widget::{button, column, combo_box, container, row, svg, text},
+    widget::{button, column, container, row, svg, text},
     Alignment, Command, Element, Length,
 };
-use iced_layershell::{reexport::Anchor, Application as _};
+use iced_layershell::{
+    reexport::{Anchor, Layer},
+    Application as _,
+};
 
 struct ControlCenter {
     is_charging: bool,
@@ -98,57 +101,81 @@ impl iced_layershell::Application for ControlCenter {
         let blue_icon = format!("{}/assets/icons/bluetooth.svg", env!("CARGO_MANIFEST_DIR"));
         let plane_icon = format!("{}/assets/icons/airplane.svg", env!("CARGO_MANIFEST_DIR"));
         let power_icon = format!("{}/assets/icons/power-mode.svg", env!("CARGO_MANIFEST_DIR"));
+        let fan_icon = format!("{}/assets/icons/fan.svg", env!("CARGO_MANIFEST_DIR"));
 
-        // TODO: Network Manager
+        // TODO: Network Manager, add power profile dropdown menu, change background to transparent
 
-        let rec_button = button(
-            row![
-                svg(svg::Handle::from_path(power_icon)).width(25).height(25),
-                column![
-                    text("Power Mode").font(styling::font::SF_PRO_BOLD).size(16),
-                    text(styling::format::kebab_to_title_case(&self.power_mode))
+        let circular_button = |icon_path| {
+            button(svg(svg::Handle::from_path(icon_path)).width(25).height(25))
+                .style(styling::style::Button::Circular)
+                .padding(17)
+        };
+
+        let rectangular_button = |title, subtitle, icon_path, message| {
+            button(
+                row![
+                    svg(svg::Handle::from_path(icon_path)).width(25).height(25),
+                    column![
+                        text(title).font(styling::font::SF_PRO_BOLD).size(16),
+                        text(subtitle)
+                    ]
                 ]
-            ]
-            .spacing(20)
-            .align_items(Alignment::Center),
-        )
-        .width(250)
-        .padding([10, 20, 10, 20]);
+                .spacing(20)
+                .align_items(Alignment::Center),
+            )
+            .on_press(message)
+            .width(250)
+            .padding([10, 20, 10, 20])
+        };
 
-        container(column![
-            row![
+        container(
+            column![
+                row![
+                    container(
+                        row![
+                            svg(svg::Handle::from_path(battery_icon))
+                                .width(25)
+                                .height(25),
+                            column![
+                                text(format!("{}%", self.percentage))
+                                    .font(styling::font::SF_PRO_BOLD),
+                                text(styling::format::seconds_to_hour_minute(self.time_to_empty))
+                            ],
+                        ]
+                        .spacing(10)
+                        .align_items(Alignment::Center),
+                    )
+                    .width(Length::Fill),
+                    container(
+                        row![
+                            circular_button(&wifi_icon),
+                            circular_button(&blue_icon),
+                            circular_button(&plane_icon),
+                        ]
+                        .spacing(10)
+                    )
+                ]
+                .align_items(Alignment::Center),
                 container(
-                    row![
-                        svg(svg::Handle::from_path(battery_icon))
-                            .width(25)
-                            .height(25),
-                        column![
-                            text(format!("{}%", self.percentage)).font(styling::font::SF_PRO_BOLD),
-                            text(styling::format::seconds_to_hour_minute(self.time_to_empty))
-                        ],
-                    ]
-                    .spacing(10)
-                    .align_items(Alignment::Center),
-                )
-                .width(Length::Fill),
-                container(
-                    row![
-                        button(svg(svg::Handle::from_path(wifi_icon)).width(25).height(25))
-                            .style(styling::style::Button::Circular)
-                            .padding(17),
-                        button(svg(svg::Handle::from_path(blue_icon)).width(25).height(25))
-                            .style(styling::style::Button::Circular)
-                            .padding(17),
-                        button(svg(svg::Handle::from_path(plane_icon)).width(25).height(25))
-                            .style(styling::style::Button::Circular)
-                            .padding(17),
+                    column![
+                        rectangular_button(
+                            "Power Mode",
+                            styling::format::kebab_to_title_case(&self.power_mode),
+                            &power_icon,
+                            Message::PowerModeSelected
+                        ),
+                        rectangular_button(
+                            "Fan Profile",
+                            "Silent".to_string(),
+                            &fan_icon,
+                            Message::PowerModeSelected
+                        ),
                     ]
                     .spacing(10)
                 )
             ]
-            .align_items(Alignment::Center),
-            container(column![rec_button])
-        ])
+            .spacing(10),
+        )
         .style(styling::style::Container::HeavyRounded)
         .padding(32)
         .width(iced::Length::Fill)
@@ -159,12 +186,14 @@ impl iced_layershell::Application for ControlCenter {
 
 fn main() -> Result<(), iced_layershell::Error> {
     ControlCenter::run(iced_layershell::settings::Settings {
+        id: Some(String::from("control_center")),
         antialiasing: true,
         default_font: styling::font::SF_PRO,
         layer_settings: iced_layershell::settings::LayerShellSettings {
-            size: Some((475, 375)),
+            layer: Layer::Top,
             anchor: Anchor::Right | Anchor::Top,
             margins: (40 + 15, 10, 0, 0),
+            size: Some((475, 375)),
             ..Default::default()
         },
         ..Default::default()
